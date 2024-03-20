a6b1efa46f292df84c79be4c604db69aae129ea6
Angel Stoykov <angelsto@abv.bg>
2024-03-20 16:55:52 +0200

    - added booking controller

---
+++ MotoBalkans/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MotoBalkans.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        public async Task<IActionResult> Book(int motorcycleId)
+        {
+            return View();
+        }
+    }
+}
